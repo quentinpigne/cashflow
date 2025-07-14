f4003892767cf66539b388c9814fa156057c49e7
Quentin Pigné <quentin.pigne@gmail.com>
2025-07-14 11:42:19 +0200

    Using model_config attribute instead of Config nested class

--- packages/backend/app/core/config.py
+++ packages/backend/app/core/config.py
@@ -1,13 +1,12 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+
     PROJECT_NAME: str
     PROJECT_VERSION: str
     DATABASE_URL: str
-
-    class Config:
-        env_file = ".env"
 
 
 settings = Settings()
